cd57a93db62c963b1fe02fa59786c4407ba5a76a
Kabilashini-Uki <kabilashini.uki.stu@dreamspace.academy>
2025-12-17 17:10:33 +0530

    miniproject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+      
+        <nav className="glass-navbar">
+          <div className="nav-container">
+            <Link to="/" className="logo"><span style={{fontSize:'30px'}}> MoodWaves</span></Link>
+            <div className="nav-links">
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+            </div>
+          </div>
+        </nav>
+
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+        
+        <footer className="footer">
+          <h4> MoodWaves - Let music heal your soul </h4>
+        </footer>
+
+      </div>
+    </Router>
   );
 }
 
